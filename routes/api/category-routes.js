1358c66aba41e955c63d4c80f8359dc41610b8d6
Kimmy <kimberlieguillaume@gmail.com>
2023-04-29 18:22:23 -0400

    updating category id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: { id: req.params.id },
     include: [Product],
-    attributes: ['id', 'category_name', 'product_name', 'price', 'stock', 'category_id']
+    attributes: ['category_name', 'product_name', 'price', 'stock']
   })
     .then((categoryData) => {
       if (!categoryData) {
